c91b8117a30c573caa546d054c29c4b9afbce067
Stephen S. Saldana <StephenTheDev@gmail.com>
2022-08-22 09:27:17 -0700

    -initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const connectDB = require('./db')
+const path = require('path')
+
+// initialize express
+const app = express()
+
+// connect db 
+connectDB()
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
